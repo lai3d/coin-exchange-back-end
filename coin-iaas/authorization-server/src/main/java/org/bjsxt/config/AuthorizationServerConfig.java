987938fee04cbf5d0b94dbaf5924bbebdec0c113
Larry Lai <cgpanda.sg@gmail.com>
2021-10-16 22:27:01 +0800

    Use JWT token

--- coin-iaas/authorization-server/src/main/java/org/bjsxt/config/AuthorizationServerConfig.java
+++ coin-iaas/authorization-server/src/main/java/org/bjsxt/config/AuthorizationServerConfig.java
@@ -2,6 +2,7 @@ package org.bjsxt.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,6 +12,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 @EnableAuthorizationServer
@@ -45,12 +49,27 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService)
-                .tokenStore(redisTokenStore());
+                .tokenStore(jwtTokenStore())
+                .tokenEnhancer(jwtAccessTokenConverter());
         super.configure(endpoints);
     }
 
-    public TokenStore redisTokenStore() {
+    private TokenStore redisTokenStore() {
         return new RedisTokenStore(redisConnectionFactory);
     }
+
+    private TokenStore jwtTokenStore() {
+        JwtTokenStore jwtTokenStore = new JwtTokenStore(jwtAccessTokenConverter());
+        return jwtTokenStore;
+    }
+    
+    private JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        ClassPathResource classPathResource = new ClassPathResource("coinexchange.jks");
+        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(classPathResource, "coinexchange".toCharArray());
+        tokenConverter.setKeyPair(keyStoreKeyFactory.getKeyPair("coinexchange", "coinexchange".toCharArray()));
+        return tokenConverter;
+    }
+    
 
 }
