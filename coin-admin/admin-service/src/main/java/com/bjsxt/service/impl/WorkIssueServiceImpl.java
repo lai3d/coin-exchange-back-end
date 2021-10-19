739190b9c2bf50a92357104f8eb112c38a0046c7
Larry Lai <cgpanda.sg@gmail.com>
2021-10-20 00:55:31 +0800

    Work issue apis

--- coin-admin/admin-service/src/main/java/com/bjsxt/service/impl/WorkIssueServiceImpl.java
+++ coin-admin/admin-service/src/main/java/com/bjsxt/service/impl/WorkIssueServiceImpl.java
@@ -1,13 +1,56 @@
 package com.bjsxt.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.bjsxt.domain.WorkIssue;
 import com.bjsxt.mapper.WorkIssueMapper;
 import com.bjsxt.service.WorkIssueService;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
 @Service
 public class WorkIssueServiceImpl extends ServiceImpl<WorkIssueMapper, WorkIssue> implements WorkIssueService{
 
+    /**
+     * 条件分页查询工单列表
+     *
+     * @param page      分页参数
+     * @param status    工单的状态
+     * @param startTime 查询的工单创建起始时间
+     * @param endTime   查询的工单创建截至时间
+     * @return
+     */
+    @Override
+    public Page<WorkIssue> findByPage(Page<WorkIssue> page, Integer status, String startTime, String endTime) {
+        return page(page, new LambdaQueryWrapper<WorkIssue>()
+                .eq(status != null, WorkIssue::getStatus, status)
+                .between(
+                        !StringUtils.isEmpty(startTime) && !StringUtils.isEmpty(endTime),
+                        WorkIssue::getCreated,
+                        startTime, endTime + " 23:59:59")
+        );
+    }
+
+
+    /**
+     * 前台系统查询客户工单
+     *
+     * @param page
+     * @return
+     */
+    @Override
+    public Page<WorkIssue> getIssueList(Page<WorkIssue> page,Long userId) {
+        return page(page,new LambdaQueryWrapper<WorkIssue>()
+                        .eq(WorkIssue::getUserId,userId)
+//                                            .eq(WorkIssue::getStatus,1)
+        );
+    }
 }
