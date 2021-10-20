36eb6240b3e627ac525774f76d6f7346ecba8fdc
Larry Lai <cgpanda.sg@gmail.com>
2021-10-20 12:49:03 +0800

    Config apis

--- coin-admin/admin-service/src/main/java/com/bjsxt/service/ConfigService.java
+++
@@ -1,8 +0,0 @@
-package com.bjsxt.service;
-
-import com.bjsxt.domain.Config;
-import com.baomidou.mybatisplus.extension.service.IService;
-public interface ConfigService extends IService<Config>{
-
-
-}
